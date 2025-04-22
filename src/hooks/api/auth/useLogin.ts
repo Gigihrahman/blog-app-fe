119e5b1eabebecb4b71df6392c4777a3615c7e3b
Gigihrahman <gigihrahmanditta@gmail.com>
2025-04-22 14:22:58 +0700

    feat: keep login

--- src/hooks/api/auth/useLogin.ts
+++ src/hooks/api/auth/useLogin.ts
@@ -1,18 +1,30 @@
 "use client";
 import { axiosInstance } from "@/lib/axios";
+import { useAuthStore } from "@/stores/auth";
 import type { User } from "@/types/user";
 import { useMutation } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
+interface Response extends User {
+  accesToken: string;
+}
 const useLogin = () => {
+  const router = useRouter();
+  const { onAuthSuccess } = useAuthStore();
   return useMutation({
     mutationFn: async (payload: Pick<User, "email" | "password">) => {
-      const { data } = await axiosInstance.post("/auth/login", payload);
+      const { data } = await axiosInstance.post<Response>(
+        "/auth/login",
+        payload,
+      );
       return data;
     },
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Login Success");
+      onAuthSuccess({ user: data, accesToken: data.accesToken });
+      router.push("/");
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data.message);
