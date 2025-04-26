2843caabecce5ae3ac08ffa11bdaf5f89bc34d65
Gigihrahman <gigihrahmanditta@gmail.com>
2025-04-26 12:21:31 +0700

    feat: implement blog creation functionality with API integration

---
+++ src/hooks/api/blog/useCreateblog.ts
@@ -0,0 +1,40 @@
+"use client";
+import { axiosInstance } from "@/lib/axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+interface CreateBlogPayload {
+  title: string;
+  category: string;
+  description: string;
+  thumbnail: File | null;
+  content: string;
+}
+const useCreateBlog = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (payload: CreateBlogPayload) => {
+      const createBlogForm = new FormData();
+      createBlogForm.append("title", payload.title);
+      createBlogForm.append("description", payload.description);
+      createBlogForm.append("category", payload.category);
+      createBlogForm.append("thumbnail", payload.thumbnail!);
+      createBlogForm.append("content", payload.content);
+      const { data } = await axiosInstance.post("/blogs", createBlogForm);
+      return data;
+    },
+    onSuccess: async () => {
+      toast.success("Create blog Success");
+      await queryClient.invalidateQueries({ queryKey: ["blogs"] });
+      router.push("/");
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(error.response?.data.message);
+    },
+  });
+};
+
+export default useCreateBlog;
