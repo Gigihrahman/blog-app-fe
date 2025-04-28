152d1ac7777e334193ec935b63da2e4e2e455673
Gigihrahman <gigihrahmanditta@gmail.com>
2025-04-28 15:47:53 +0700

    feat:add auto logout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import { Toaster } from "sonner";
 import NuqsProvider from "@/providers/NuqsProvider";
 import Navbar from "@/components/Navbar";
+import TokenProvider from "@/providers/TokenProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,8 +34,10 @@ export default function RootLayout({
       >
         <ReactQueryProvider>
           <NuqsProvider>
-            <Navbar />
-            {children}
+            <TokenProvider>
+              <Navbar />
+              {children}
+            </TokenProvider>
           </NuqsProvider>
         </ReactQueryProvider>
         <Toaster richColors position="top-right" />
